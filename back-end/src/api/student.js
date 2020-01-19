361d988c8945b837da68b7eef348ff939f76821b
Jesse Smick <janka102@gmail.com>
2020-01-19 04:20:23 -0800

    Save images and parse likelihoods

--- back-end/src/api/student.js
+++ back-end/src/api/student.js
@@ -1,8 +1,17 @@
 const express = require("express");
+const fs = require("fs");
 const vision = require("@google-cloud/vision");
+const path = require("path");
 
 const router = express.Router();
 const visionClient = new vision.ImageAnnotatorClient();
+const attentionFolder = path.join(
+  __dirname,
+  "..",
+  "..",
+  "public",
+  "attentions"
+);
 
 // Things to consider:
 // - What student is this?
@@ -15,21 +24,82 @@ router.post("/faceDetect", (req, res) => {
     picture = picture.slice(picture.indexOf("base64,") + "base64,".length);
   }
 
+  const content = Buffer.from(picture, "base64");
+
   visionClient
-    .faceDetection({
-      image: {
-        content: Buffer.from(picture, "base64")
-      }
-    })
+    .faceDetection({ image: { content } })
     .then(([response]) => {
       const faces = response.faceAnnotations;
-      console.log(faces);
-      res.json(faces);
+      if (faces.length < 1) {
+        return res.json(null);
+      }
+
+      const {
+        rollAngle,
+        panAngle,
+        tiltAngle,
+        detectionConfidence,
+        joyLikelihood,
+        sorrowLikelihood,
+        angerLikelihood,
+        surpriseLikelihood
+      } = faces[0];
+
+      const face = {
+        roll: rollAngle,
+        pan: panAngle,
+        tilt: tiltAngle,
+        detectionConfidence,
+        joy: likelihoodToNumber(joyLikelihood),
+        sorrow: likelihoodToNumber(sorrowLikelihood),
+        anger: likelihoodToNumber(angerLikelihood),
+        surprise: likelihoodToNumber(surpriseLikelihood)
+      };
+
+      const filename = `${Date.now()}-${(10 + Math.random() * 90) | 0}`;
+      fs.writeFile(
+        path.join(attentionFolder, filename + ".png"),
+        content,
+        err => {
+          console.log(err);
+        }
+      );
+      fs.writeFile(
+        path.join(attentionFolder, filename + ".json"),
+        JSON.stringify(face, null, 2),
+        err => {
+          console.log(err);
+        }
+      );
+      res.json(face);
     })
     .catch(err => {
       res.status(500);
       res.json(err);
     });
 });
+
+/**
+ * Convert a likelihood string to a number between 0-1
+ *
+ * @param {string} likelihood
+ * @returns number
+ */
+function likelihoodToNumber(likelihood) {
+  switch (likelihood) {
+    case "VERY_UNLIKELY":
+      return Math.random() * 0.2;
+    case "UNLIKELY":
+      return 0.2 + Math.random() * 0.2;
+    case "POSSIBLE":
+      return 0.4 + Math.random() * 0.2;
+    case "LIKELY":
+      return 0.6 + Math.random() * 0.2;
+    case "VERY_LIKELY":
+      return 0.8 + Math.random() * 0.2;
+    default:
+      return 0;
+  }
+}
 
 module.exports = router;
