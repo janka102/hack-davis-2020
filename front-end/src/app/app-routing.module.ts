2414f13ad2c06c0341c38d2a2a26777cff07ff1d
Diane Tobit <dianetobit@Dianes-MacBook-Pro.local>
2020-01-19 00:10:42 -0800

    add routing

--- front-end/src/app/app-routing.module.ts
+++ front-end/src/app/app-routing.module.ts
@@ -1,11 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { VideoPlayerComponent } from './video-player/video-player.component';
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: "class/:classId/lecture/:lectureId",
+    component: VideoPlayerComponent
+  }
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, { useHash: true })],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
